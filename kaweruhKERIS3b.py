d7876c73226e2aacebcf8ec08a546377cb396bfe
FajarWibisono <73801942+FajarWibisono@users.noreply.github.com>
2025-01-14 18:16:31 +0700

    Update kaweruhKERIS3b.py
    ganti mixtral dengan groq-merdeka-11B

--- kaweruhKERIS3b.py
+++ kaweruhKERIS3b.py
@@ -107,7 +107,7 @@ def initialize_rag():
         # 4.5 Menginisialisasi LLM (ChatGroq)
         llm = ChatGroq(
             temperature=0.54,
-            model_name="mixtral-8x7b-32768",
+            model_name="qroq-merdeka-11B",
             max_tokens=1024
         )
 
